5455edc5144e85b8a79cc5378c9843ea3103e511
jiwon <jiwon936626@naver.com>
2025-09-20 01:27:29 +0900

    feat: complete section 5-5

--- app/login.tsx
+++ app/login.tsx
@@ -1,5 +1,7 @@
+import { initializeKakaoSDK } from "@react-native-kakao/core";
+import { login as kakaoLogin, me } from "@react-native-kakao/user";
 import { Redirect, router } from "expo-router";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import {
   Pressable,
   StyleSheet,
@@ -16,9 +18,21 @@ export default function Login() {
   const { user, login } = useContext(AuthContext);
   const isLoggedIn = !!user;
 
+  const openKakaoLogin = async () => {
+    const result = await kakaoLogin();
+    console.log("result: ", result);
+    const user = await me();
+    console.log("user: ", user);
+  };
+
+  useEffect(() => {
+    initializeKakaoSDK("2d6a890ed320e205e6de1a17f7230856");
+  }, []);
+
   if (isLoggedIn) {
     return <Redirect href="/(tabs)" />;
   }
+
   return (
     <View
       style={{
@@ -35,11 +49,34 @@ export default function Login() {
       <Pressable style={styles.loginButton} onPress={login}>
         <Text style={styles.loginButtonText}>Login</Text>
       </Pressable>
+      <Pressable
+        style={[styles.loginButton, styles.kakaoLoginButton]}
+        onPress={openKakaoLogin}
+      >
+        <Text style={[styles.loginButtonText, styles.kakaoLoginButtonText]}>
+          Kakao Login
+        </Text>
+      </Pressable>
+      <Pressable
+        style={[styles.loginButton, styles.appleLoginButton]}
+        onPress={login}
+      >
+        <Text style={styles.loginButtonText}>Apple Login</Text>
+      </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  kakaoLoginButtonText: {
+    color: "black",
+  },
+  appleLoginButton: {
+    backgroundColor: "black",
+  },
+  kakaoLoginButton: {
+    backgroundColor: "yellow",
+  },
   loginButton: {
     backgroundColor: "blue",
     padding: 10,
